486e105d139424b97dae82ec55db8ae2adac4abb
githubyc <326831169@qq.com>
2019-03-30 18:38:04 +0800

    测试

---
+++ src/test/test.java
@@ -0,0 +1,20 @@
+/*  
+ * Copyright @ 2019 项目名称
+ * test 下午6:21:57  
+ * All right reserved.  
+ *  
+*/  
+    
+package test;
+
+/**  
+ * @desc: 
+ * @author: yc  
+ * @createTime: 2019年3月30日 下午6:21:57  
+ * @history:  
+ * @version: v1.0    
+ */
+
+public class test {
+
+}
